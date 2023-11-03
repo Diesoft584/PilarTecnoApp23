b38584696dc7ee5b21fa3161591ad83b95d55c05
Diego <oviedodiego584@gmail.com>
2023-11-03 15:00:15 -0300

    Subiendo lo correspondiente a la actividad tp3

--- src/screens/Map/Maps.js
+++ src/screens/Map/Maps.js
@@ -1,56 +1,196 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {SafeAreaView, Dimensions, StyleSheet, Text, View} from 'react-native';
 import Header from '../../componets/Header';
+import Geolocation from 'react-native-geolocation-service';
+import {hasLocationPermission} from '../../services/locationPermissions';
+import {Image, Icon} from '@rneui/themed';
+import MapView, {Marker} from 'react-native-maps';
 
 const WIDTH = Dimensions.get('window').width;
 const HEIGHT = Dimensions.get('window').height;
 
+const ASPECT_RATIO = WIDTH / HEIGHT;
+const LATITUDE = 0.0922;
+const LONGITUDE = 0.0421;
+const LATITUDE_DELTA = 0.00422;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 const Mapas = () => {
+  const mapRef = useRef(null);
+
+  const [region, setRegion] = useState({
+    longitude: LATITUDE,
+    latitude: LONGITUDE,
+    latitude_delta: LATITUDE_DELTA,
+    longitude_delta: LONGITUDE_DELTA,
+  });
+
+  useEffect(() => {
+    hasLocationPermission();
+  }, []);
+
+  _getLocation = async () => {
+    await Geolocation.getCurrentPosition(
+      async posicion => {
+        const longitude = posicion.coords.longitude;
+        const latitude = posicion.coords.latitude;
+
+        mapRef.current.animateToRegion(
+          {
+            latitude,
+            longitude,
+            latitudeDelta: region.latitudeDelta,
+            longitudeDelta: region.longitudeDelta,
+          },
+          1000,
+        );
+
+        setRegion({
+          latitude,
+          longitude,
+          latitudeDelta: region.latitudeDelta,
+          longitudeDelta: region.longitudeDelta,
+        });
+        console.log(
+          'posicion actual... Latitud: ' +
+            `${JSON.stringify(longitude)}` +
+            ' latitud:' +
+            `${JSON.stringify(latitude)}`,
+        );
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+        forceRequestLocation: true,
+      },
+    );
+  };
+
+  const onRegionChange = r => {
+    console.log({r, ...region});
+  };
+
+  fitCoordinates = async () => {
+    console.log('centrando mapa');
+    this._getLocation();
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <View style={{...styles.gridRow, flexDirection: 'row'}}>
-        <Text style={{fontSize: 20}}>Mapa</Text>
-      </View>
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      {region.latitude && region.longitude && (
+        <>
+          <MapView
+            ref={mapRef}
+            style={styles.map}
+            initialRegion={{
+              latitude: region.latitude || 37.78825,
+              longitude: region.longitude || -122.4324,
+              latitudeDelta: region.latitude_delta || 0.0922,
+              longitudeDelta: region.longitude_delta || 0.0421,
+            }}
+            onRegionChange={onRegionChange}
+          />
+
+          {/* <MapView
+            ref={mapRef}
+            mapType="standard"
+            style={styles.map}
+            initialRegion={region}
+            // region={this.state.region}
+            onRegionChangeComplete={onRegionChange}
+      /> */}
+
+          <View
+            style={{
+              position: 'absolute',
+              flexDirection: 'row',
+              backgroundColor: 'white',
+              borderRadius: 100,
+              width: WIDTH / 10,
+              alignSelf: 'flex-end',
+              margin: 20,
+              marginRight: 30,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Icon
+              name="crosshairs"
+              type="font-awesome"
+              color="#8d2d84"
+              size={WIDTH / 10}
+              onPress={() => fitCoordinates()}
+            />
+          </View>
+          <View style={styles.markerFixed}>
+            <Image
+              style={styles.marker}
+              source={require('../../assets/images/pin.png')}
+            />
+          </View>
+          <SafeAreaView style={styles.footer}>
+            <Text style={styles.region}>
+              longitud:
+              {JSON.stringify(region.longitude)}
+              {'\n'}latitud:
+              {JSON.stringify(region.latitude)}
+            </Text>
+          </SafeAreaView>
+        </>
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  girdContainer: {
-    flex: 1,
-    height: HEIGHT,
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  content: {
+    margin: WIDTH / 20,
+    height: WIDTH / 2.5,
+    width: WIDTH / 2.5,
+    borderRadius: 15,
+    justifyContent: 'center',
+  },
+  markerFixed: {
+    left: '50%',
+    marginLeft: -24,
+    marginTop: -48,
+    position: 'absolute',
+    top: '50%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
     width: WIDTH,
+    height: HEIGHT,
+    alignSelf: 'center',
   },
-  gridColumn: {
-    flex: 1,
-
-    alignItems: 'center',
+  marker: {
+    height: 48,
+    width: 48,
   },
-  gridRow: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  gridButton: {
-    width: WIDTH * 0.4,
-    height: WIDTH * 0.4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    elevation: 3,
-  },
-  buttonTitle: {
-    fontSize: 18,
-    color: 'white',
-    fontWeight: '500',
-    elevation: 3,
-  },
-  container: {
-    flex: 1,
-    height: '100%',
+  footer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    bottom: 30,
+    position: 'absolute',
     width: '100%',
-    justifyContent: 'center',
+  },
+  region: {
+    color: '#fff',
+    lineHeight: 20,
+    margin: 20,
+    alignSelf: 'center',
   },
 });
 
